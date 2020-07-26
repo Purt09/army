d6eaaf70d2d51e78b6f619ef61c635265b7c45f7
Purtv <Alexpurtv@gmail.com>
2020-07-26 07:31:15 +0300

    БОЛЬШОЕ ОБНОВЛЕНИЕ

---
+++ common/fixtures/UserFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace common\fixtures;
+
+use core\entities\User\User;
+use yii\test\ActiveFixture;
+
+class UserFixture extends ActiveFixture
+{
+    public $modelClass = User::class;
+}
